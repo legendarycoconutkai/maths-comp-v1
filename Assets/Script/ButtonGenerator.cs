476ba1992f43adee1e8523643721b52def31e95a
junhubdotcom <furnoyong@gmail.com>
2024-01-24 15:43:22 +0800

    Update ButtonGenerator.cs

--- Assets/Script/ButtonGenerator.cs
+++ Assets/Script/ButtonGenerator.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class ButtonGenerator : MonoBehaviour
 {
@@ -10,13 +12,25 @@ public class ButtonGenerator : MonoBehaviour
 
     private void OnEnable()
     {
+        string[] operators = {"+", "-", "*", "/", "^", "%"};
+
         for (int i = 0; i<24; i++)
         {
             int counter = i+1;
             GameObject newButton = Instantiate(buttonPrefab, buttonParent.transform);
-            newButton.GetComponent<ButtonInfo>().buttonText.text = "Button"+i;
+            string buttonText = operators[Random.Range(0,5)];
+            newButton.GetComponent<ButtonInfo>().buttonText.text = buttonText;
+
+            Button button = newButton.GetComponent<Button>();
             newButton.GetComponent<Button>().onClick.RemoveAllListeners();
-            newButton.GetComponent<Button>().onClick.AddListener(() => { Debug.Log("Button" + counter + "Clicked");}) ;
+            newButton.GetComponent<Button>().onClick.AddListener(() => { OnButtonClick(counter, buttonText); }) ;
         }
+
+
+    }
+
+    private void OnButtonClick(int buttonNumber, string buttonText)
+    {
+        Debug.Log("Button " + buttonNumber + " Clicked Text: " + buttonText);
     }
 }
