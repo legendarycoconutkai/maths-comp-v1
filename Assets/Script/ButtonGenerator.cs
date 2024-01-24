f176b1c5b70ecc31b4454cd4b659ae28a4107ef0
legendarycoconutkai <73057952+legendarycoconutkai@users.noreply.github.com>
2024-01-24 18:15:39 +0800

    show equation

--- Assets/Script/ButtonGenerator.cs
+++ Assets/Script/ButtonGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,9 +11,14 @@ public class ButtonGenerator : MonoBehaviour
     public GameObject buttonPrefab;
     public GameObject buttonParent;
 
+    public TextMeshProUGUI displayText;
+    private string currentInput = "";
+    private double result = 0.0;
+    public GameObject equalButton;
+
     private void OnEnable()
     {
-        string[] operators = {"+", "-", "*", "/", "^", "%"};
+        string[] operators = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/", "^"};
 
         for (int i = 0; i<24; i++)
         {
@@ -23,14 +29,39 @@ public class ButtonGenerator : MonoBehaviour
 
             Button button = newButton.GetComponent<Button>();
             newButton.GetComponent<Button>().onClick.RemoveAllListeners();
-            newButton.GetComponent<Button>().onClick.AddListener(() => { OnButtonClick(counter, buttonText); }) ;
+            newButton.GetComponent<Button>().onClick.AddListener(() => { OnButtonClick(buttonText); });
         }
 
-
+        equalButton.GetComponent<Button>().onClick.AddListener(() => { CalculateResult();});
     }
-
-    private void OnButtonClick(int buttonNumber, string buttonText)
+    /*private void OnButtonClick(int buttonNumber, string buttonText)
     {
         Debug.Log("Button " + buttonNumber + " Clicked Text: " + buttonText);
+    }*/
+
+    public void OnButtonClick(string buttonValue)
+    {
+        currentInput += buttonValue;
+        UpdateDisplay();
+    }
+    public void CalculateResult()
+    {
+        try
+        {
+            result = System.Convert.ToDouble(new System.Data.DataTable().Compute(currentInput, ""));
+
+            currentInput = result.ToString();
+
+            UpdateDisplay();
+        }
+        catch (System.Exception)
+        {
+            currentInput = "Error";
+            UpdateDisplay();
+        }
+    }
+    private void UpdateDisplay()
+    {
+        displayText.text = currentInput;
     }
 }
