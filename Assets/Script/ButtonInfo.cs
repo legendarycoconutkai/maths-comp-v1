f4f450d416de27b4a7f66a05c31390c8629bf60c
legendarycoconutkai <73057952+legendarycoconutkai@users.noreply.github.com>
2024-01-24 14:45:35 +0800

    Update ButtonInfo.cs

--- Assets/Script/ButtonInfo.cs
+++ Assets/Script/ButtonInfo.cs
@@ -7,28 +7,4 @@ using UnityEngine.UI;
 public class ButtonInfo : MonoBehaviour
 {
     public TextMeshProUGUI buttonText;
-
-    // Create a Dictionary to store strings and their corresponding boolean values
-    Dictionary<string, bool> operators = new Dictionary<string, bool>()
-    {
-    // Add elements to the dictionary
-    {"+", true},
-    {"-", true},
-    {"*", true},
-    {"/", true},
-    {"0", true},
-    {"1", true},
-    {"2", true},
-    {"3", true},
-    {"4", true},
-    {"5", true},
-    {"6", true},
-    {"7", true},
-    {"8", true},
-    {"9", true},
-    {"0", true},
-    {"!", false},
-    {"^2", true},
-    {"sqrt", true}
-    };
 }
