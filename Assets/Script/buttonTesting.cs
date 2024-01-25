eaadc9d4b1548e7c9856dd9f98c8b37bcffe0adc
legendarycoconutkai <73057952+legendarycoconutkai@users.noreply.github.com>
2024-01-25 12:09:44 +0800

    button generation update

--- Assets/Script/buttonTesting.cs
+++ Assets/Script/buttonTesting.cs
@@ -8,15 +8,46 @@ using TMPro;
 
 public class buttonTesting : MonoBehaviour
 {
-    public GameObject button;
-    public TextMeshProUGUI buttonText;
+    public GameObject[] button;
+    public TextMeshProUGUI[] buttonText;
     string[] operators = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/", "^" };
+
+    public TextMeshProUGUI displayText;
+    private string currentInput = "";
+    private double result = 0.0;
 
     private void OnEnable()
     {
-        button.SetActive(true);
-        string text = operators[Random.Range(0, operators.Length)];
-        buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
-        buttonText.text = text;
+        for(int i = 0; i < button.Length; i++)
+        {
+            button[i].SetActive(true);
+            string text = operators[Random.Range(0, operators.Length)];
+            buttonText[i].text = text;
+        }
+    }
+    public void firstClick(string buttonValue)
+    {
+        currentInput += buttonValue;
+        UpdateDisplay();
+    }
+    public void CalculateResult()
+    {
+        try
+        {
+            result = System.Convert.ToDouble(new System.Data.DataTable().Compute(currentInput, ""));
+
+            currentInput = result.ToString();
+
+            UpdateDisplay();
+        }
+        catch (System.Exception)
+        {
+            currentInput = "Error";
+            UpdateDisplay();
+        }
+    }
+    private void UpdateDisplay()
+    {
+        displayText.text = currentInput;
     }
 }
